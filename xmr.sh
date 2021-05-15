2e2198d4b7b068398c8cb1a9b847395b80758e54
joezau <37699577+joezau@users.noreply.github.com>
2021-05-15 14:20:04 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+#xmr.sh
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-bionic-x64.tar.gz
+tar -xvf xmrig-6.8.1-bionic-x64.tar.gz
+./xmrig-6.8.1/xmrig -o pool.supportxmr.com:443 -u 48urR78Bj6pXE9gfQCKpJJMHYBCEmMxLtikwjriTgFDdGabrTP18wfTH5AqhQbhv3UBnpymMue7cm4sJR2mKbhxeKYgYXeD.a1 -k --tls -p x -t 1
